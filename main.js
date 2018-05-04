ab91394abebc572bbaa7cb8678a0dc52c244f418
Douglas Paul <douglas.w.paul@gmail.com>
2018-05-04 00:18:45 -0400

    Load the cards from a CSV file

--- main.js
+++ main.js
@@ -1,42 +1,66 @@
-var shuffledCards = shuffle(cards)
-//      .filter(function(card) {
-//         return card.types.includes(ATTACK);
-//     })
-    .slice(0,10)
-    .sort(compareCards);
+fetch('cards.csv').then(function(response) {
+    return response.text();
+}).then(function(responseText) {
+    const ACTION = 'action';
+    const ATTACK = 'attack';
+    const DURATION = 'duration';
+    const REACTION = 'reaction';
+    const TREASURE = 'treasure';
+    const VICTORY = 'victory';
 
-for (var listCard of shuffledCards) {
-    printCard(listCard);
-}
+    const BASE_GAME = 'Base Game';
+    const INTRIGUE = 'Intrigue';
+    const SEASIDE = 'Seaside';
 
-// Returns a shuffled copy of the array
-function shuffle(array) {
-    // An inefficient but concise way to shuffle an array
-    return array.map(it => ({item: it, n: Math.random()})).sort((a,b) => a.n - b.n).map(it => it.item);
-}
+    const cards = d3.csvParse(responseText);
 
-function compareCards(cardA, cardB) {
-    if (cardA.cost < cardB.cost) {
-        return -1;
-    } else if (cardB.cost < cardA.cost) {
-        return 1;
-    } else {
-        return cardA.name.localeCompare(cardB.name);
+    var shuffledCards = shuffle(cards)
+    //      .filter(function(card) {
+    //         return card.types.includes(ATTACK);
+    //     })
+        .slice(0,10)
+        .sort(compareCards);
+
+    for (var listCard of shuffledCards) {
+        printCard(listCard);
     }
-}
 
-function printCard(listCard) {
-    var liElement = document.createElement('li');
-    liElement.classList.add('card');
-    liElement.appendChild(createCoinElement(listCard.cost));
-    liElement.appendChild(document.createTextNode(' ' + listCard.name + ' (' + listCard.expansion + ')'));
-    var ulElement = document.getElementById('cardList');
-    ulElement.appendChild(liElement);
-}
+    // Returns a shuffled copy of the array
+    function shuffle(array) {
+        // An inefficient but concise way to shuffle an array
+        return array.map(it => ({item: it, n: Math.random()})).sort((a,b) => a.n - b.n).map(it => it.item);
+    }
 
-function createCoinElement(value) {
-    const coinSpan = document.createElement('span');
-    coinSpan.classList.add('coin');
-    coinSpan.innerHTML = `<span>${value}</span>`
-    return coinSpan;
-}
+    function compareCards(cardA, cardB) {
+        if (cardA.cost < cardB.cost) {
+            return -1;
+        } else if (cardB.cost < cardA.cost) {
+            return 1;
+        } else {
+            return cardA.name.localeCompare(cardB.name);
+        }
+    }
+
+    function printCard(listCard) {
+        var liElement = document.createElement('li');
+        liElement.classList.add('card');
+        liElement.appendChild(createCoinElement(listCard.cost));
+        liElement.appendChild(document.createTextNode(' ' + listCard.name + ' (' + listCard.expansion + ')'));
+        var ulElement = document.getElementById('cardList');
+        ulElement.appendChild(liElement);
+    }
+
+    function createCoinElement(value) {
+        const coinSpan = document.createElement('span');
+        coinSpan.classList.add('coin');
+        coinSpan.innerHTML = `<span>${value}</span>`
+        return coinSpan;
+    }
+})
+.catch(function() {
+    if (document.location.protocol.startsWith('http')) {
+        document.body.textContent = "Failed to load card data. See console for details.";
+    } else {
+        document.body.textContent = "Failed to load card data. Try loading the page via HTTP or HTTPS.";
+    }
+});
